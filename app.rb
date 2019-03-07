30401fd5b2ae19f826ad7b7f43abd3c284c2152b
Sara Walker <sarawall1187@gmail.com>
2019-03-07 16:27:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "New App using Sintatra and Shotgun"
   end
 
 end
